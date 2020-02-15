5aa977ea1109b6c3e8043a9349d2f97677d168d2
Casey Ferguson <cferg019@gmail.com>
2020-02-15 11:42:14 -0500

    fixed handlebars file

--- public/js/script.js
+++ public/js/script.js
@@ -5,53 +5,55 @@ var logTrigger = document.querySelector("button#logbtn.trigger");
 var signTrigger = document.querySelector("button#signbtn.trigger");
 var closeButton = document.querySelector(".close-button");
 
-function toggleModal() {	
-    modal.classList.toggle("show-modal");	
-}	
-
-function windowOnClick(event) {	$(document).ready(function () {
-    if (event.target === modal) {	
-        toggleModal();	
-    }	
+function toggleModal() {
+    modal.classList.toggle("show-modal");
 }
-                                                  
+
+function windowOnClick(event) {
+    $(document).ready(function () {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    });
+}
+
 $(document).ready(function () {
 
     $(".watchedForm").on("submit", function (event) {
-      event.preventDefault();
-  
-      var mediaID = $(this).children(".mediaID").val();
-      console.log(mediaID);
-      $.ajax({
-        method: "PUT",
-        url: "/watch/" + mediaID
-      }).then(function (data) {
-        // reload page to display new media proper column
-        location.reload();
-      });
-    });
- 
-    $("#mediaSubmitForm").on("submit", function (event) {
-      if ($("#mediaName").val() === "") {
         event.preventDefault();
-        $("#error").text("Please fill out all fields.")
-      }
+
+        var mediaID = $(this).children(".mediaID").val();
+        console.log(mediaID);
+        $.ajax({
+            method: "PUT",
+            url: "/watch/" + mediaID
+        }).then(function (data) {
+            // reload page to display new media proper column
+            location.reload();
+        });
+    });
+
+    $("#mediaSubmitForm").on("submit", function (event) {
+        if ($("#mediaName").val() === "") {
+            event.preventDefault();
+            $("#error").text("Please fill out all fields.")
+        }
     })
-  });
-  
-if(logTrigger) {
+});
+
+if (logTrigger) {
     logTrigger.addEventListener("click", toggleModal)
 }
 
-if(signTrigger) {
+if (signTrigger) {
     signTrigger.addEventListener("click", toggleModal)
 }
 
-if(closeButton) {
+if (closeButton) {
     closeButton.addEventListener("click", toggleModal)
 }
 
-if(window) {
+if (window) {
     window.addEventListener("click", windowOnClick)
 }
 

8b334535bca57b5bdfa9892a7f7b1f36187633ce
Kali Pulkkinen <kapulkkinen@gmail.com>
2020-02-19 21:12:04 -0500

    subtle changes on homepage pop ups

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE NewMedia; 
+
+INSERT INTO NewMedia (name, genre, mediaType, watched)
+VALUES ("Jaws", "Action", "Movie", FALSE);
+
+INSERT INTO NewMedia (name, genre, mediaType, watched)
+VALUES ("Space Jam", "Comedy", "Movie", FALSE);
+
+INSERT INTO NewMedia (name, genre, mediaType, watched)
+VALUES ("Forrest Gump", "Drama", "Movie", FALSE);
+
+INSERT INTO NewMedia (name, genre, mediaType, watched)
+VALUES ("Mr. Robot", "Sci-Fi", "Television", FALSE);

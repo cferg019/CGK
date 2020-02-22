007e887716331ab1a31d262bb97d92082bdc8dba
Casey Ferguson <cferg019@gmail.com>
2020-02-22 14:01:02 -0500

    changed configs for heroku

--- config/config.js
+++ config/config.js
@@ -1,26 +1,19 @@
 require('dotenv').config();  // this line is important!
 module.exports = {
     "development": {
-        "username": process.env.LOCAL_USERNAME,
-        "password": process.env.LOCAL_PASSWORD,
-        "database": process.env.LOCAL_DB,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
         "host": "127.0.0.1",
         "dialect": "mysql",
         "migrationStorageTableName": "sequelize_meta"
     },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql",
-        "operatorsAliases": false
-    },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
         "dialect": "mysql",
         "operatorsAliases": false
     }

94e2a579d0c629bc8c3d7e014f7e7419ad95b329
George Ferguson <george@fergusondevelopmentgroup.com>
2020-02-21 22:31:17 -0500

    Added third party api call, and touched up front-end UX, added form validation

--- controllers/watchControllers.js
+++ controllers/watchControllers.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var axios = require('axios');
 var db = require('../models')
 
 var router = express.Router();
@@ -33,17 +33,48 @@ router.get('/watch', function (req, res) {
 });
 
 // post route -> back to index
-router.post('/watch/create', function (req, res) {
-    db.NewMedia.create({
-        name: req.body.name,
-        genre: req.body.genre,
-        // mediaType: req.body.mediaType,
-        watched: false,
-        UserId: req.user.id
-    }).then(function (show) {
-        console.log('created show', show);
-        res.redirect('/');
-    })
+router.post('/watch', function (req, res) {
+
+    var queryURL = "https://www.omdbapi.com/?t=" + req.body.name + "&apikey=trilogy";
+
+    axios.get(queryURL)
+        // Get the OMDB Data
+        .then(response => {
+            console.log('heres the data back from omdb', response.data)
+            return {
+                posterUrl: response.data.Poster,
+                rating: response.data.imdbRating,
+                year: response.data.Year
+            }
+        })
+        // Something went wrong getting ombd data, but let's just skip it
+        .catch(err => {
+            console.log('the ombd call failed', err)
+            return {};
+        })
+        // Create the Media object
+        .then(omdbData => {
+            console.log('got to this part', omdbData)
+            return db.NewMedia.create({
+                name: req.body.name,
+                genre: req.body.genre,
+                mediaType: req.body.mediaType,
+                watched: false,
+                UserId: req.user.id,
+                rating: omdbData.rating,
+                posterUrl: omdbData.posterUrl,
+                year: omdbData.year
+            })
+        })
+        // Respond back to the browser.
+        .then(function (show) {
+            console.log('created show', show);
+            res.json(show);
+        })
+        .catch(err => {
+            console.log('we got an error', err)
+            res.status(500).send("Internal Server Error.");
+        })
 });
 
 // put route -> back to index
